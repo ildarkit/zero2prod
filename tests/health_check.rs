faf8732bae4ab55d83f82ea37c373d4b6d72816f
Ildar Shamiev <shamief@yandex.ru>
2022-06-24 10:39:37 +0300

    query to database for subscription

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -24,7 +24,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
     let config = get_configuration().expect("Failed to read configuration.");
     let connection_string = config.database.connection_string();
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connet to Postgres.");
 
@@ -39,6 +39,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .await
         .expect("Failed to execute request.");
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("select email, name from subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
