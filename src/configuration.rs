bc387394a53c0fc2055ba5de1fff7098e2d071f9
Ildar Shamiev <shamief@yandex.ru>
2022-06-25 16:42:34 +0300

    production and local configurations

--- src/configuration.rs
+++ src/configuration.rs
@@ -4,7 +4,13 @@ use secrecy::Secret;
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -16,8 +22,50 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+/// The possible runtime environment for our application.
+pub enum Environment {
+    Local,
+    Production,
+}
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let settings = config::Config::builder().add_source(config::File::with_name("configuration"));
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    let settings = config::Config::builder()
+        .add_source(config::File::from(configuration_directory.join("base")).required(true));
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    let settings = settings.add_source(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    );
+
     match settings.build() {
         Ok(config) => config.try_deserialize(),
         Err(e) => Err(e),
