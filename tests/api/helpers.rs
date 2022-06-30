984b6e9ffa2ef626de1f02f12c7a9054e3eb589d
Ildar Shamiev <shamief@yandex.ru>
2022-06-30 12:19:08 +0300

    fix bug: application.port instead of database.port

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -31,7 +31,7 @@ pub async fn spawn_app() -> TestApp {
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
         c.database.database_name = Uuid::new_v4().to_string();
-        c.database.port = 0;
+        c.application.port = 0;
         c
     };
 
