1457df73b9df0bc4262128fb30613ca436bfc5fb
Ildar Shamiev <shamief@yandex.ru>
2022-07-07 12:02:29 +0300

    using a cookie for error messaging instead of query parameters

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,10 +1,7 @@
 use crate::authentication::{self, AuthError, Credentials};
 use crate::routes::error_chain_fmt;
-use crate::startup::HmacSecret;
 use actix_web::error::InternalError;
 use actix_web::{http::header::LOCATION, web, HttpResponse};
-use hmac::{Hmac, Mac};
-use secrecy::ExposeSecret;
 use secrecy::Secret;
 use sqlx::PgPool;
 use std::fmt::Formatter;
@@ -22,7 +19,6 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    secret: web::Data<HmacSecret>,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -42,19 +38,8 @@ pub async fn login(
                 AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
                 AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
             };
-            let query_string = format!("error={}", urlencoding::Encoded::new(e.to_string()));
-            let hmac_tag = {
-                let mut mac =
-                    Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes())
-                        .unwrap();
-                mac.update(query_string.as_bytes());
-                mac.finalize().into_bytes()
-            };
             let response = HttpResponse::SeeOther()
-                .insert_header((
-                    LOCATION,
-                    format!("/login?{}&tag={:x}", query_string, hmac_tag),
-                ))
+                .insert_header((LOCATION, "/login"))
                 .finish();
             Err(InternalError::from_response(e, response))
         }
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -10,4 +10,7 @@ async fn an_error_flash_message_is_set_on_failure() {
     let response = app.post_login(&login_body).await;
     assert_eq!(response.status().as_u16(), 303);
     helpers::assert_is_redirect_to(&response, "/login");
+
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
+    assert_eq!(flash_cookie.value(), "Authentication failed");
 }
