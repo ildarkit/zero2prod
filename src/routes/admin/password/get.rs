1598b7e1a52193729ed54aff2cdc35016b7aecae
Ildar Shamiev <shamief@yandex.ru>
2022-07-09 16:11:43 +0300

    added change user password form

---
+++ src/routes/admin/password/get.rs
@@ -0,0 +1,42 @@
+use actix_web::http::header::ContentType;
+use actix_web::HttpResponse;
+
+pub async fn change_password_form() -> Result<HttpResponse, actix_web::Error> {
+    Ok(HttpResponse::Ok().content_type(ContentType::html()).body(
+        r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equev="content-type" content="text/html"; charset="utf-8">
+    <title>Change Password</title>
+</head>
+    <body>
+        <form action="/admin/password" method="post">
+            <label>Current password
+                <input
+                    type="password"
+                    placeholder="Enter current password"
+                    name="current_password"
+                >
+            </label>
+            <br>
+            <label>New password
+                <input
+                    type="password"
+                    placeholder="Enter new password"
+                    name="new_password"
+                >
+            </label>
+            <br>
+            <label>
+                <input
+                    type="password"
+                    placeholder="Type the new password again"
+                    name="new_password_check"
+                >
+            </label>
+            <br>
+            <p><a href="/admin/dashboard">&lt;-Back</a></p>
+    </body>
+</html>"#,
+    ))
+}
--- src/startup.rs
+++ src/startup.rs
@@ -106,6 +106,11 @@ pub async fn run(
             .route("/login", web::get().to(routes::login_form))
             .route("/login", web::post().to(routes::login))
             .route("/admin/dashboard", web::get().to(routes::admin_dashboard))
+            .route(
+                "/admin/password",
+                web::get().to(routes::change_password_form),
+            )
+            .route("/admin/password", web::post().to(routes::change_password))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
