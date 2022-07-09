ecef16563a897c1a1fd454c88314eb208ff4fc3f
Ildar Shamiev <shamief@yandex.ru>
2022-07-09 10:38:45 +0300

    rotating session token when user logs in to prevent a session fixation attacks

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -31,6 +31,7 @@ pub async fn login(
     match authentication::validate_credentials(credentials, &pool).await {
         Ok(user_id) => {
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+            session.renew();
             session
                 .insert("user_id", user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
