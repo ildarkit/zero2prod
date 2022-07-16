d3cd452f04670ea1412716fcefef28032faa74cc
Ildar Shamiev <shamief@yandex.ru>
2022-07-16 12:00:00 +0300

    fix bug

--- migrations/20220716084515_create_idempotency_table.sql
+++ migrations/20220716084515_create_idempotency_table.sql
@@ -1,10 +1,10 @@
 CREATE TYPE header_pair AS (
   name TEXT,
   value BYTEA
-)
+);
 
 CREATE TABLE idempotency (
-  user_id uuid NOT NULL REFERENCES user(user_id),
+  user_id uuid NOT NULL REFERENCES users(user_id),
   idempotency_key TEXT NOT NULL,
   response_status_code SMALLINT NOT NULL,
   response_headers header_pair[] NOT NULL,
