ebf87cc3d3943a130c3bde12208e47b919f1ffc8
Ildar Shamiev <shamief@yandex.ru>
2022-07-18 09:18:58 +0300

    try_processing return two state of request processing, save_response update the response

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -1,7 +1,7 @@
 use crate::authentication::UserId;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
-use crate::idempotency::{self, IdempotencyKey};
+use crate::idempotency::{self, IdempotencyKey, NextAction};
 use crate::utils;
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
@@ -41,11 +41,15 @@ pub async fn publish_newsletter(
         idempotency_key,
     } = form.0;
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(utils::e400)?;
-    if let Some(saved_response) = idempotency::get_saved_response(&pool, &idempotency_key, *user_id)
+    match idempotency::try_processing(&pool, &idempotency_key, *user_id)
         .await
         .map_err(utils::e500)?
     {
-        return Ok(saved_response);
+        NextAction::StartProcessing => {}
+        NextAction::ReturnSavedResponse(saved_response) => {
+            success_message().send();
+            return Ok(saved_response);
+        }
     }
     let subscribers = get_confirmed_subscribers(&pool)
         .await
@@ -67,12 +71,16 @@ pub async fn publish_newsletter(
             }
         }
     }
-    FlashMessage::info("Newsletter issue started successfully.").send();
+    success_message().send();
     let response = utils::see_other("/admin/newsletters");
     let response = idempotency::save_response(&pool, &idempotency_key, *user_id, response)
         .await
         .map_err(utils::e500)?;
     Ok(response)
+}
+
+fn success_message() -> FlashMessage {
+    FlashMessage::info("Newsletter issue started successfully.")
 }
 
 #[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
